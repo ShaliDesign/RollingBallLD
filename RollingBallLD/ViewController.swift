8ea68b590fd2a25ddbddb2f78042a78879ca6344
LinhLinhshawn-dShawnD <linhshawn-d@LinhShawnDs-MacBook-Pro.local>
2017-04-13 01:10:08 +0700

    first commit

--- RollingBallLD/ViewController.swift
+++ RollingBallLD/ViewController.swift
@@ -10,16 +10,78 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    var ball = UIImageView()
+    var radians = CGFloat()
+    var ballRadious = CGFloat()
+ 
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        addBall()
+        //rollBall()
+        RightToLeft()
+       
+      let time = Timer.scheduledTimer(timeInterval: 0.02, target: self, selector: #selector(rollBall), userInfo: nil, repeats: true)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    
+    
+    
+    
+    func addBall()
+    
+    {
+        let mainViewSize = self.view.bounds.size
+        ball = UIImageView(image: UIImage(named: "ball.png"))
+        ballRadious = 32.0
+        ball.center = CGPoint(x: ballRadious, y: mainViewSize.height * 0.5)
+        self.view.addSubview(ball)
     }
+    
+    func rollBall()
+    {
+        let deltaAngle: CGFloat = 0.1
+        radians = radians + deltaAngle
+        ball.transform = CGAffineTransform(rotationAngle: radians)
+        ball.center = CGPoint(x: ball.center.x + ballRadious * deltaAngle, y: ball.center.y)
+        
+//        ball.center = CGPoint(x: ball.center.x + ballRadious * deltaAngle, y: ball.center.y + ballRadious * deltaAngle)
+        
+    }
+        
+    func RightToLeft()
+    {
+        
+        UIView.animate(withDuration: 4, animations: {
+
+            self.ball.center = CGPoint(x: self.view.bounds.size.width, y: self.view.bounds.size.height * 0.5)
+            
+        }) { (finished) in
+       
+                self.ball.center = CGPoint(x: self.ballRadious, y: self.view.bounds.size.height * 0.5)
+            
+            print("this is the end")
+        }
+
+ 
+    }
+    
+        
+//            self.ball.transform = self.ball.transform.scaledBy(x: -1, y: 1).concatenating(CGAffineTransform(rotationAngle: 0))
+
+        
+//            UIView.animate(withDuration: 4, animations: {
+//                self.ball.center = CGPoint(x: self.ball.center.x, y: self.ball.center.y)})
+        
+        
+        }
 
 
-}
+
+
+
 
